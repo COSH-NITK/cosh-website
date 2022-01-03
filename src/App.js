bceb4fea5f7a659086d037ce89b99d0b4d515826
aadilkhalifa <aadilkhalifa@gmail.com>
2022-01-03 22:49:35 +0530

    added how we work page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Navbar from './Components/Navbar/Navbar'
 import Footer from './Components/Footer/Footer'
 import About from './Components/About/About'
 import Contact from './Components/Contact/Contact'
+import HowWeWork from './Components/HowWeWork/HowWeWork'
 
 import { BrowserRouter as Router, Routes ,Route, Navigate } from 'react-router-dom';
 
@@ -19,6 +20,7 @@ function App() {
         <Route path='/blog' element={<Blogs/>} />
         <Route path='/about' element={<About/>} />
         <Route path='/contact' element={<Contact/>} />
+        <Route path='/how-we-work' element={<HowWeWork/>} />
         <Route exact path="/" element={<Navigate replace to="/home" />}>
         </Route>
         </Routes>
---
+++ src/Components/HowWeWork/HowWeWork.jsx
@@ -0,0 +1,24 @@
+import './HowWeWork.scss'
+
+import React from 'react'
+
+
+import il1 from '../../Assets/il_howwework1.svg';
+
+function HowWeWork() {
+    return (
+        <div className="howWeWorkDiv">
+            <div className="section1">
+                <h1>How we<br />work</h1>
+                <img src={il1} className="il1" alt="Illustration" />
+            </div>
+            <div className="section2">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Penatibus ipsum consequat cursus amet blandit nullam amet. Egestas in vulputate elementum nam bibendum. Aliquet at elementum, fames amet lacus, ultrices suspendisse scelerisque. Elit felis et, volutpat eget in at maecenas lectus netus. Iaculis morbi neque, fringilla quam porttitor lacinia. Iaculis duis arcu lectus laoreet gravida fames donec cursus mauris. Lacinia in et felis ornare interdum elementum. Ultrices penatibus ullamcorper amet justo. Rhoncus facilisis dignissim dui porttitor purus facilisis dignissim integer pulvinar.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Penatibus ipsum consequat cursus amet blandit nullam amet. Egestas in vulputate elementum nam bibendum. Aliquet at elementum, fames amet lacus, ultrices suspendisse scelerisque. Elit felis et, volutpat eget in at maecenas lectus netus. Iaculis morbi neque, fringilla quam porttitor lacinia. Iaculis duis arcu lectus laoreet gravida fames donec cursus mauris. Lacinia in et felis ornare interdum elementum. Ultrices penatibus ullamcorper amet justo. Rhoncus facilisis dignissim dui porttitor purus facilisis dignissim integer pulvinar.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Penatibus ipsum consequat cursus amet blandit nullam amet. Egestas in vulputate elementum nam bibendum. Aliquet at elementum, fames amet lacus, ultrices suspendisse scelerisque. Elit felis et, volutpat eget in at maecenas lectus netus. Iaculis morbi neque, fringilla quam porttitor lacinia. Iaculis duis arcu lectus laoreet gravida fames donec cursus mauris. Lacinia in et felis ornare interdum elementum. Ultrices penatibus ullamcorper amet justo. Rhoncus facilisis dignissim dui porttitor purus facilisis dignissim integer pulvinar.</p>
+            </div>
+        </div>
+    )
+}
+
+export default HowWeWork
