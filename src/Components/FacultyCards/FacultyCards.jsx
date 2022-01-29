5c20fcacd31018c720223bb8bf60ce70db9f4688
aadilkhalifa <aadilkhalifa@gmail.com>
2022-01-29 14:13:16 +0300

    ⚡ Faculty card shows optional links, right avatar

--- src/Components/FacultyCards/FacultyCards.jsx
+++ src/Components/FacultyCards/FacultyCards.jsx
@@ -2,9 +2,10 @@ import './FacultyCards.scss';
 import avatar1 from './avatar1.svg';
 import avatar2 from './avatar2.svg';
 import avatar3 from './avatar3.svg';
+import avatarF1 from './avatarF1.svg';
 
 import React from 'react'
-import { GrMail } from 'react-icons/gr';
+import { AiFillMail, AiFillLinkedin } from 'react-icons/ai';
 
 function FacultyCards({
     people = [],
@@ -25,18 +26,34 @@ function FacultyCard({
     person = {},
 }) {
 
-    const avatars = [avatar1, avatar2, avatar3];
+    const avatarsM = [avatar1, avatar2, avatar3];
+    const avatarsF = [avatarF1];
 
 
     return (
         <div className="facultyCardDiv" >
             {/* <img src="https://images.unsplash.com/photo-1584999734482-0361aecad844?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80" alt="" /> */}
-            <img src={avatars[getRandomInt(3)]} alt="" />
+            {
+                person.gender && person.gender == "F" 
+                ? <img src={avatarsF[getRandomInt(avatarsF.length)]} alt="" />
+                : <img src={avatarsM[getRandomInt(avatarsM.length)]} alt="" />
+            }
             <div>
                 <h4>{person.name}</h4>
                 <p className="cardTitle">{person.title}</p>
                 <p className="cardOrg">{person.org}</p>
-                <a href = {"mailto: "+person.email}><GrMail size="25" /></a>
+                <div className="iconsRow">
+                    {
+                        !person.email ? null :
+                        <a href = {"mailto: "+person.email}><AiFillMail size="25" /></a>
+
+                    }
+                    {
+                        !person.linkedin ? null :
+                        <a href = {person.linkedin} target="_blank"><AiFillLinkedin size="25" /></a>
+
+                    }
+                </div>
             </div>
         </div>
     )
