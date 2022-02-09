64d55a4bcde675164faa37bcb3e7ec73249b6251
aadilkhalifa <aadilkhalifa@gmail.com>
2022-02-09 22:01:17 +0300

    ⚡ Changed domain card colors

--- src/Components/DomainCards/DomainCards.jsx
+++ src/Components/DomainCards/DomainCards.jsx
@@ -20,7 +20,7 @@ function DomainCards() {
                             <style>
                                 
                             </style> 
-                            <rect width="294" height="294" fill="#3D9481"/>
+                            <rect width="294" height="294" fill="#5C1FDE"/>
                             <circle id="circle1" cx="64.4363" cy="63.3344" r="11.4363" fill="#101014" fill-opacity="0.2"/>
                             <circle id="circle2" cx="92.1243" cy="63.3344" r="11.4363" fill="#101014" fill-opacity="0.2"/>
                             <circle id="circle3" cx="119.812" cy="63.3344" r="11.4363" fill="#101014" fill-opacity="0.2"/>
@@ -74,12 +74,12 @@ function DomainCards() {
                 <div className="domainCard">
                     <div className="domainImg5">
                         <svg id="svg5" width="294" height="294" viewBox="0 0 294 294" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <rect id="bg" width="294" height="294" fill="#7D3E56"/>
-                            <rect x="90.1965" y="89.1965" width="114.607" height="114.607" stroke="#C4C4C4" stroke-width="8"/>
-                            <rect id="box1" x="72" y="129.077" width="37.4273" height="37.4273" fill="#C4C4C4"/>
-                            <rect id="box2" x="128.786" y="71" width="37.4273" height="37.4273" fill="#C4C4C4"/>
-                            <rect id="box3" x="128.786" y="184.573" width="37.4273" height="37.4273" fill="#C4C4C4"/>
-                            <rect id="box4" x="185.573" y="129.077" width="37.4273" height="37.4273" fill="#C4C4C4"/>
+                            <rect id="bg" width="294" height="294" fill="#5C1FDE"/>
+                            <rect x="90.1965" y="89.1965" width="114.607" height="114.607" stroke="#FFFFFF" stroke-width="8"/>
+                            <rect id="box1" x="72" y="129.077" width="37.4273" height="37.4273" fill="#FFFFFF"/>
+                            <rect id="box2" x="128.786" y="71" width="37.4273" height="37.4273" fill="#FFFFFF"/>
+                            <rect id="box3" x="128.786" y="184.573" width="37.4273" height="37.4273" fill="#FFFFFF"/>
+                            <rect id="box4" x="185.573" y="129.077" width="37.4273" height="37.4273" fill="#FFFFFF"/>
                         </svg>
 
                     </div>
@@ -92,7 +92,7 @@ function DomainCards() {
                 <div className="domainCard">
                     <div className="domainImg4">
                     <svg  viewBox="0 0 294 294" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <rect width="294" height="294" fill="#9C3D3B"/>
+                        <rect width="294" height="294" fill="#5C1FDE"/>
                         <rect id="circle1" x="69" y="70.1304" width="30.5217" height="30.5217" fill="black" fill-opacity="0.3"/>
                         <rect id="circle5" x="69" y="110.826" width="30.5217" height="30.5217" fill="black" fill-opacity="0.3"/>
                         <rect id="circle9" x="69" y="152.652" width="30.5217" height="30.5217" fill="black" fill-opacity="0.3"/>
@@ -124,7 +124,7 @@ function DomainCards() {
                     <style>
                         
                     </style>
-                    <rect width="294" height="294" fill="#507A53"/>
+                    <rect width="294" height="294" fill="#5C1FDE"/>
                     <rect x="99" y="81.8459" width="97" height="139.692" stroke="white" stroke-width="8"/>
                     <rect x="115" y="67" width="64" height="12" fill="white"/>
                     <rect id="box1" x="114.615" y="97.4614" width="64.6154" height="19.6154" fill="white"/>
@@ -143,7 +143,7 @@ function DomainCards() {
                 <div className="domainCard">
                     <div className="domainImg8">
                     <svg  viewBox="0 0 294 294" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <rect width="294" height="294" fill="#87619F"/>
+                        <rect width="294" height="294" fill="#5C1FDE"/>
                         <rect x="68" y="68" width="158" height="158" rx="76" fill="black" fill-opacity="0.2"/>
                         <rect id="square" x="91" y="91" width="112" height="112" fill="black" fill-opacity="0.2"/>
                         <path id="triangle" d="M147 92L202.426 203H91.5745L147 92Z" fill="white"/>
@@ -159,7 +159,7 @@ function DomainCards() {
                 <div className="domainCard">
                     <div className="domainImg3">
                     <svg  viewBox="0 0 294 294" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <rect width="294" height="294" fill="#255861"/>
+                    <rect width="294" height="294" fill="#5C1FDE"/>
                     <circle cx="147" cy="148.077" r="83.5" stroke="white"/>
                     <circle cx="147" cy="148.077" r="60.8846" stroke="white"/>
                     <circle cx="147" cy="148.077" r="39.3462" stroke="white"/>
@@ -183,7 +183,7 @@ function DomainCards() {
                 <div className="domainCard">
                     <div className="domainImg6">
                         <svg  viewBox="0 0 294 294" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <rect width="294" height="294" fill="#3B837E"/>
+                            <rect width="294" height="294" fill="#5C1FDE"/>
                             <rect id="rect1" x="55.1865" y="294" width="62.456" height="22.8497" transform="rotate(-90 55.1865 294)" fill="black" fill-opacity="0.3"/>
                             <rect id="rect2" x="102.409" y="184.321" width="62.456" height="22.8497" fill="black" fill-opacity="0.3"/>
                             <rect id="rect3" x="187.715" y="161.471" width="62.456" height="22.8497" transform="rotate(-90 187.715 161.471)" fill="black" fill-opacity="0.3"/>
@@ -215,7 +215,7 @@ function DomainCards() {
                             <style>
                                 
                             </style>
-                            <rect width="294" height="294" fill="#485A77"/>
+                            <rect width="294" height="294" fill="#5C1FDE"/>
                             <circle id="circle0" cx="43.5" cy="146.5" r="10.5" fill="rgba(255,255,255,0)"/>
                             <circle id="circle1" cx="69.5" cy="146.5" r="10.5" fill="white"/>
                             <circle id="circle2" cx="95.5" cy="146.5" r="10.5" fill="white"/>
@@ -237,7 +237,7 @@ function DomainCards() {
                 <div className="domainCard">
                     <div className="domainImg2">
                         <svg  viewBox="0 0 294 294" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <rect width="294" height="294" fill="#665E7F"/>
+                            <rect width="294" height="294" fill="#5C1FDE"/>
                             <rect x="57" y="56" width="181" height="181" fill="#101014" fill-opacity="0.1"/>
                             <rect x="75.7241" y="74.7241" width="143.552" height="143.552" fill="#101014" fill-opacity="0.1"/>
                             <rect x="93.2" y="92.2" width="108.6" height="108.6" fill="#101014" fill-opacity="0.1"/>
