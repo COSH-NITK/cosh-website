1ab2cf15ed626eae6430506d3451e3acc46ab8d9
aadilkhalifa <aadilkhalifa@gmail.com>
2022-09-17 18:22:19 +0530

    ⚡ Updated postman page content

--- src/Pages/Postman/Postman.jsx
+++ src/Pages/Postman/Postman.jsx
@@ -1,9 +1,8 @@
 import React, {useEffect} from 'react'
 import {Link} from 'react-router-dom'
+import {Helmet} from "react-helmet";
 
 import './Postman.scss';
-import il1 from '../../Assets/il_postman1.svg';
-
 
 function Postman() {
 
@@ -13,6 +12,9 @@ function Postman() {
 
   return (
     <div className="postmanDiv">
+        <Helmet>
+            <title>Postman Program</title>
+        </Helmet>
         <div className="postmanSection1Container">
             <div className="postmanSection1">
                 <h1>COSH NITK <span>Postman</span> Classroom Program</h1>
@@ -27,57 +29,61 @@ function Postman() {
             </div>
         </div>
         <div className="postmanSection3">
-            <h2>About</h2>
+            {/* <h2>About</h2>
             <p>Application Programming Interface (API) is a software-to-software interface that enables two applications to exchange data with each other. In a world increasingly dominated by digital, APIs are an integral part of our daily lives. They are a part of almost everything we do on the Internet. Each time we use an application like Instagram, send an instant message or check the latest news on our phones, we're using an API!</p>
             <p>APIs allow the line of business users and IT to leverage software and applications to increase productivity and improve the bottom line. From social collaboration tools to more innovative approaches to customer outreach, taking advantage of APIs can prove dividends within the enterprise.</p>
             <p>To promote API Literacy and API-First Principles amongst the students of NITK Surathkal, Postman, a startup turned Unicorn (and a Pioneer in API Industry), is offering a Certification Course on “API-based Programming”. The first round of the course offering will start in August 2022. The mode of the course will be online with live lectures and tutorials for all the modules, and will be free of cost! It will give an opportunity to all the students at NITK Surathkal to delve into API-based literacy, which is currently being offered to leading institutions globally!</p>
 
-            <hr />
+            <hr /> */}
 
-            <h2>Details of the Certificate Course</h2>
-            <p>The certificate course on “API-based Programming” is a three module course with the following structure:</p>
+            {/* <h2>Details of the Certificate Course</h2> */}
+            <h2>Certification: Postman API Fundamentals Student Expert</h2>
+
+            <h3>Duration</h3>
+            <p>Self paced (Recommended) - 1 week</p>
+
+            <h3>Description</h3>
+            <p>This certification includes two course modules that take students from knowing nothing about APIs to mastering the foundations of APIs and working with Postman. </p>
             <div className="row">
                 <div className="rowItem">
-                    <h3>API 101: API Fundamentals</h3>
+                    <h3>Course 1: Intro to APIs and Postman</h3>
                     <strong>Topics:</strong>
                     <ul>
                         <li>What are APIs?</li>
-                        <li>Servers & Clients</li>
-                        <li>Making HTTP requests to an API</li>
+                        <li>API types</li>
+                        <li>API First philosophy</li>
+                        <li>What is Postman?</li>
+                        <li>Creating Postman workspaces and collections</li>
+                        <li>Making GET, POST, PATCH, DELETE requests using a real Library API</li>
                         <li>Understanding API responses</li>
-                        <li>Submitting data to APIs</li>
-                        <li>Using query parameters to refine requests</li>
+                        <li>Authorizing requests with API Keys</li>
+                        <li>Basic scripting in Postman</li>
+                        <li>Calling APIs with code</li>
                     </ul>
-                    <strong>Assignment:</strong>
-                    <p>Librarian simulation using a Library API</p>
+                    <strong>Learning methods</strong>
+                    <ul>
+                        <li>Text-based learning with images</li>
+                        <li>Knowledge checks</li>
+                        <li>Working hands on in Postman</li>
+                    </ul>
                 </div>
                 <div className="rowItem">
-                    <h3>API 102: Building Workflows with APIs</h3>
+                    <h3>Course 2: Postman API Fundamentals Student Expert Certification</h3>
                     <strong>Topics:</strong>
                     <ul>
-                        <li>Using Authorization to make requests</li>
-                        <li>Calling a real-world REST API</li>
-                        <li>Using variables and chaining multiple API calls to build a workflow </li>
-                        <li>Visualizing response data</li>
+                        <li>Completing API challenges in Postman on the theme of sports</li>
+                        <li>GET, POST, PATCH, DELETE requests</li>
+                        <li>Request authorization</li>
+                        <li>Basic scripting</li>
                     </ul>
-                    <strong>Assignment:</strong>
-                    <p>Build a workflow that uses the Spotify API to recommend music based on three seed artists</p>
-                </div>
-                <div className="rowItem">
-                    <h3>API 103: Building Applications with APIs</h3>
-                    <strong>Topics:</strong>
+                    <strong>Learning methods</strong>
                     <ul>
-                        <li>Using Glitch as a web server for your app</li>
-                        <li>Building a web app using Node/Express, HTML and JavaScript</li>
-                        <li>Translating workflows into code</li>
-                        <li>Debugging apps with Postman</li>
+                        <li>Challenges are presented in API responses, in Postman</li>
                     </ul>
-                    <strong>Assignment:</strong>
-                    <p>Build a full stack application (Node.js/Express/HTML/JS) that uses the Spotify API to recommend music based on three seed artists</p>
                 </div>
             </div>
 
-            <h3>Course Features</h3>
+            {/* <h3>Course Features</h3>
             <ul>
                 <li>Each module shall have 1-2 hours of lecture delivery, followed by an assignment</li>
                 <li>Tutorials hours for each module where students will get assistance to solve the assignments</li>
@@ -86,17 +92,18 @@ function Postman() {
                 <li>Free of cost for all the students of NITK Surathkal</li>
                 <li>No Pre-requisites</li>
                 <li>Open to students from all disciplines and batches</li>
-            </ul>
+            </ul> */}
 
             <h3>What will students get?</h3>
-            <ul>
+            Upon completion, students will earn a Postman API Fundamentals Student Expert badge that they can share on LinkedIn and social media to prove their API and Postman skills.
+            {/* <ul>
                 <li>Postman Student Expert Badge (on completion of additional student expert module)</li>
                 <li>Certificate of Course Completion (Printable Soft Copy only)</li>
                 <li>Opportunity to learn API-based programming that is currently being offered to leading institutions globally!</li>
-            </ul>
+            </ul> */}
 
-            <h3>Mode of Delivery</h3>
-            Live lectures and tutorial sessions for every module.
+            {/* <h3>Mode of Delivery</h3>
+            Live lectures and tutorial sessions for every module. */}
 
             {/* <hr />
 
@@ -122,7 +129,7 @@ function Postman() {
 
             <hr />
 
-            <h2>Course Logistics</h2>
+            {/* <h2>Course Logistics</h2>
             <ul>
                 <li>Students can complete all three modules of the course with the help of live lectures and tutorial sessions that shall be conducted for each module. The recordings of the live sessions will be made available to all the registered students.</li>
                 <li>Students can also take the help of the video recordings (created by Postman) that are associated with each module over the Postman platform.</li>
@@ -131,16 +138,15 @@ function Postman() {
                 <li>After finishing all three modules of the course, students can register for the student expert program and claim their Student Expert Badge. The details are given below.</li>
             </ul>
 
-            <hr />
+            <hr /> */}
 
-            <h2>Student Expert Badge</h2>
+            {/* <h2>Student Expert Badge</h2>
             <ul>
                 <li>Students who complete all three modules of the API course can register for the student expert program. They can complete this additional student expert module and claim their Student Expert Badge.</li>
                 <li>The badge is a digital badge and will be shared with you via email. This badge can be displayed over social media platforms, such as LinkedIn.</li>
-                {/* <li>Click Here to register.</li>                 */}
             </ul>
 
-            <hr />
+            <hr /> */}
 
             {/* <h2>Contact Us</h2> */}
             <p className="contactCaption">For all your queries related to this certificate course, please contact us.</p>
